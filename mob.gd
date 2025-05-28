479dfe9d7a744d7c0e27013208367ce505e6a52e
Arlo Purcell <arlo@purcell.live>
2025-05-28 08:29:30 -0400

    removing old tiles and adding some AI to the crab

--- mob.gd
+++ mob.gd
@@ -1,9 +1,22 @@
 extends RigidBody2D
 
+@export var speed = 200
+
+var velocity = Vector2.RIGHT
+
 @export var max_health = 100
 var current_health = max_health
 
 signal health_changed(old_value: int, new_value: int)
+
+var is_attacking = false
+@export var attack_distance = 120
+@export var attack_velocity = 0
+
+@export var ai_freq_frames = 10
+@export var ai_freq_offset = 0
+
+var visible_enemies = {}
 
 func take_damage(damage: int) -> void:
 	var old_health = current_health
@@ -15,3 +28,82 @@ func _on_health_changed(old_value: int, new_value: int) -> void:
 	$HealthBar.value = (current_health as float / max_health as float) * 100.0
 	if new_value <= 0:
 		queue_free()
+		
+func ai_process():
+	if is_attacking:
+		# if attacking do nothing, motion continues
+		pass
+	else:
+		var enemy = nearest_enemy()
+		if enemy == null:
+			$NavigationAgent2D.target_position = global_position
+		else:
+			if global_position.distance_to(enemy.global_position) < attack_distance:
+				print("starting attack")
+				$NavigationAgent2D.target_position = global_position
+				is_attacking = true
+				# If in position to attack, do it. start animations and set motion
+				var enemy_direction = global_position.direction_to(enemy.global_position)
+				if abs(enemy_direction.x) > abs(enemy_direction.y):
+					if enemy_direction.x > 0:
+						$BodySprite.flip_h = false
+						$BodySprite.play("attack_right")
+						$AttackAnimation.play("attack_right")
+						velocity = Vector2.RIGHT * attack_velocity
+					else:
+						$BodySprite.flip_h = true
+						$BodySprite.play("attack_right")
+						$AttackAnimation.play("attack_right")
+						velocity = Vector2.LEFT * attack_velocity
+				else:
+					if enemy_direction.y > 0:
+						$BodySprite.flip_h = false
+						$BodySprite.play("attack_down")
+						$AttackAnimation.play("attack_down")
+						velocity = Vector2.DOWN * attack_velocity
+					else:
+						$BodySprite.flip_h = false
+						$BodySprite.play("attack_up")
+						$AttackAnimation.play("attack_up")
+						velocity = Vector2.UP * attack_velocity
+			else:
+				# else if in vision range of player, navigate towards them
+				$NavigationAgent2D.target_position = enemy.global_position
+				#var path_point = $NavigationAgent2D.get_next_path_position()
+				#velocity = global_position.direction_to(path_point) * speed
+
+func nearest_enemy() -> PhysicsBody2D:
+	var closest_enemy = null
+	var closest_distance = INF
+	for enemy in visible_enemies.keys():
+		var distance = global_position.distance_to(enemy.global_position)
+		if distance < closest_distance:
+			closest_distance = distance
+			closest_enemy = enemy
+	return closest_enemy
+
+func _physics_process(delta):
+	if Engine.get_process_frames() % ai_freq_frames == ai_freq_offset:
+		ai_process()
+	if not $NavigationAgent2D.is_navigation_finished():
+		var path_point = $NavigationAgent2D.get_next_path_position()
+		velocity = global_position.direction_to(path_point) * speed
+		$BodySprite.animation = "move"
+		$BodySprite.flip_h = velocity.x < 0
+	elif not is_attacking:
+		$BodySprite.play("idle")
+	move_and_collide(velocity * delta)
+
+
+func _on_attack_animation_finished(anim_name: StringName) -> void:
+	print("done attack")
+	is_attacking = false
+
+
+func _on_vision_collider_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player_mob"):
+		visible_enemies[body] = null
+
+func _on_vision_collider_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player_mob"):
+		visible_enemies.erase(body)
